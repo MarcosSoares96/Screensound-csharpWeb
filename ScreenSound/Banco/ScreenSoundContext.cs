e1439d00166d74e32d17debff46036db0d91f127
MarcosSoares96 <marcos_santos_soares@hotmail.com>
2025-02-07 16:08:21 -0300

    1. Implementação de relacionamento entre tabelas; 2. Utilização do pacote Proxies para carregamento lento de informações; 3. Refatoração do código do menu para utilização dos dados da tabela de músicas.

--- ScreenSound/Banco/ScreenSoundContext.cs
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -19,7 +19,9 @@ namespace ScreenSound.Banco
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder
+                .UseSqlServer(connectionString)
+                .UseLazyLoadingProxies();
         }
 
 
--- ScreenSound/Modelos/Musica.cs
+++ ScreenSound/Modelos/Musica.cs
@@ -1,6 +1,6 @@
 ﻿namespace ScreenSound.Modelos;
 
-internal class Musica
+public class Musica
 {
     public Musica(string nome)
     {
@@ -10,11 +10,22 @@ internal class Musica
     public string Nome { get; set; }
     public int Id { get; set; }
     public int? AnoLancamento { get; set; }
+    public virtual Artista? Artista { get; set; }
 
     public void ExibirFichaTecnica()
     {
         Console.WriteLine($"Nome: {Nome}");
       
+    }
+
+    public void ExibirMusicaPorAnoLancamento()
+    {
+        Console.WriteLine($"Musicas {AnoLancamento}");
+
+        foreach (var musica in Musicas)
+        {
+            Console.WriteLine($"Música: {musica.Nome}");
+        }
     }
 
     public override string ToString()
