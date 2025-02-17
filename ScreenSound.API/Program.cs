fa284a0eca35d9a42c37e37ee97635c9aefd0ccb
MarcosSoares96 <marcos_santos_soares@hotmail.com>
2025-02-17 08:36:01 -0300

    1. Aplicação das boas práticas de projeto utilizando o Princípio de Responsabilidade Única do SOLID. 2. Documentação da aplicação utilizando o Swagger; 3. Implementação do padrão DTO para definir o funcionamento da transferência de dados; 4. Aplicação da refatoração para obter um código mais legível e fácil de manter

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ScreenSound.API.Endpoints;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
 using System.Data.SqlTypes;
@@ -8,62 +9,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ScreenSoundContext>();
 builder.Services.AddTransient<DAL<Artista>>();
+builder.Services.AddTransient<DAL<Musica>>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
 
-app.MapGet("/Artistas", ([FromServices] DAL<Artista> dal) => { 
+app.AddEndpointsArtistas();
+app.AddEndpointsMusicas();
 
-    return Results.Ok(dal.Listar());
-});
-
-app.MapGet("/Artistas/{nome}", ([FromServices] DAL < Artista > dal, string nome) => {
-
-    var artista =  dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
-
-    if (artista is null)
-    {
-        return Results.NotFound();
-    }
-    return Results.Ok(artista);
-
-});
-
-app.MapPost("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] Artista artista) =>
-{
-    dal.Adicionar(artista);
-    return Results.Ok();
-});
-
-
-app.MapDelete("/Artistas/{id}", ([FromServices] DAL<Artista> dal, int id) =>
-{
-    var artista = dal.RecuperarPor(a => a.Id == id);
-
-    if (artista is null)
-    {
-        return Results.NotFound();
-    }
-    dal.Deletar(artista);
-    return Results.NoContent();
-});
-
-app.MapPut("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] Artista artista) =>
-{
-    var atualizarArtista = dal.RecuperarPor(a => a.Id == artista.Id);
-
-    if (atualizarArtista is null)
-    {
-        return Results.NotFound();
-    }
-
-    atualizarArtista.Nome = artista.Nome;
-    atualizarArtista.Bio = artista.Bio;
-    atualizarArtista.FotoPerfil = artista.FotoPerfil;
-
-    dal.Atualizar(atualizarArtista);
-    return Results.Ok();
-});
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
--- ScreenSound.Shared.Modelos/Modelos/Musica.cs
+++ ScreenSound.Shared.Modelos/Modelos/Musica.cs
@@ -2,6 +2,8 @@
 
 public class Musica
 {
+    private int artistaId;
+
     public Musica(string nome)
     {
         Nome = nome;
@@ -11,6 +13,7 @@ public class Musica
     public int Id { get; set; }
     public int? AnoLancamento { get; set; }
     public virtual Artista? Artista { get; set; }
+    public int ArtistaId { get; set; }
 
     public void ExibirFichaTecnica()
     {
